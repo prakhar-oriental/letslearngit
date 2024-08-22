724241fcd6c73fbe50e651ed925479496aabf4e6
Prakhar Rathore <prakhar.r786@gmail.com>
2024-08-22 18:40:16 +0530

    adding multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply 
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1*n2;  
+System.out.println("multiply of numbers is: "+sum);  
+}  
+} 
